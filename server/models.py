9ccede985081a5b9a3013040e25a1958ef9b3156
Kirill Olar <kirill.olar26@gmail.com>
2021-07-09 10:34:38 +0600

    Define project structure

---
+++ server/models.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class ModelInput(BaseModel):
+    pass
+
+class ModelOutput(BaseModel):
+    pass
+
+class RequestBody(BaseModel):
+    pass
+
+class ResponseBody(BaseModel):
+    pass
