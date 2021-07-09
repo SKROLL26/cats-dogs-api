e1c493063e006005a2430eefd72a12aa4b42bad0
Kirill Olar <kirill.olar26@gmail.com>
2021-07-09 11:01:54 +0600

    change method from GET to POST

--- server/routes.py
+++ server/routes.py
@@ -3,6 +3,6 @@ from server.models import RequestBody, ResponseBody
 
 router = APIRouter(prefix="/api/v1/catsdogs")
 
-@router.get("/predict", response_model=ResponseBody)
+@router.post("/predict", response_model=ResponseBody)
 def predict(request_body: RequestBody):
     return None
